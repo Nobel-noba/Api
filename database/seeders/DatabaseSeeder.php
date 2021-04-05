0830bc5e525063a2cf6097733af0a1f58974c2c5
nobel <nobelmitiku@gmail.com>
2021-04-05 17:48:17 +0300

    setting up the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\models;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+//         \App\Models\User::factory(10)->create();
+         models\product::factory(50)->create();
+         models\Review::factory(300)->create();
+
     }
 }
