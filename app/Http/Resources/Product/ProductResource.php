0265aa5239e03143360df28c07d97bbe292ff0be
nobel <nobelmitiku@gmail.com>
2021-04-10 17:40:47 +0300

    adding new data in the api and manipulating it in the resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Routing\Route;
 
 class ProductResource extends JsonResource
 {
@@ -20,6 +21,12 @@ class ProductResource extends JsonResource
             'price'         =>  $this->price,
             'stock'         =>  $this->stock,
             'discount'      =>  $this->discount,
+//            the next line calculates the rating using the relationship between the product and reviews objects
+            'Rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),1) : "Not rated yet",
+            'link' => [
+//                this will place the routing link to the specified route and pass the id as parameter
+                'Reciews'=> route('review.index',$this->id)
+            ]
         ];
     }
 }
